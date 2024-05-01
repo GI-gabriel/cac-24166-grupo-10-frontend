1fca09f1eacd3d993642ebc4a72443c0f7cd9485
GI-gabriel <161953323+GI-gabriel@users.noreply.github.com>
2024-05-01 11:21:43 -0300

    Agregar menu desplegable a botón hamburguesa

---
+++ js/boton-menu.js
@@ -0,0 +1,25 @@
+
+function iniciar() {
+    var elemento = document.getElementById("menu-button");
+    elemento.addEventListener("click", mostrarMenu);
+}
+
+function mostrarMenu() {
+    var elemento = document.getElementById("main-nav-bar");
+    if (elemento.style.display == "none")
+        elemento.style.display = "block";
+    else
+        elemento.style.display = "none";
+}
+
+const mql = window.matchMedia("(max-width: 992px)");
+mql.onchange = (e) => {
+    var elemento = document.getElementById("main-nav-bar");
+    
+    if (e.matches)
+    elemento.style.display = "none";
+else
+elemento.style.display = "block";
+};
+
+window.addEventListener("load", iniciar);
