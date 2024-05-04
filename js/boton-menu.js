a2bd7ee331bb88886a5d78dcce7a8a4968805761
GI-gabriel <161953323+GI-gabriel@users.noreply.github.com>
2024-05-04 17:45:21 -0300

    feat: Se agrega animación de estado a botón menú

--- js/boton-menu.js
+++ js/boton-menu.js
@@ -7,12 +7,26 @@ function iniciar() {
 function mostrarMenu() {
     var elemento = document.getElementById("main-nav-bar");
     elemento.style.height = elemento.style.height != "220px" ? "220px" : "0";
+
+    let estado = elemento.style.height != "220px" ? true : false;
+    anim_boton(estado);
 }
 
 const mql = window.matchMedia("(max-width: 992px)");
 mql.onchange = (e) => {
     var elemento = document.getElementById("main-nav-bar");
     elemento.style.height = e.matches ? "0" : "auto";
+
+    let estado = elemento.style.height != "220px" ? true : false;
+    anim_boton(estado);
 };
 
 window.addEventListener("load", iniciar);
+
+function anim_boton(estado) {
+    var e2 = document.getElementById("nav-icon1");
+    if (estado)
+        e2.classList.remove('open');
+    else
+        e2.classList.add('open');
+}
