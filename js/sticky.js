7796a7a51ee609088badff4b8c1da80ea7583222
mikeowl <michel.b.diamant@gmail.com>
2024-05-08 17:50:21 -0300

    Añadiendo función sticky con JS. Aún falta un retoque para hacerla andar.

---
+++ js/sticky.js
@@ -0,0 +1,11 @@
+// Función para aplicar Sticky al navbar
+const navbar = document.querySelector('#title-bar-container');
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
