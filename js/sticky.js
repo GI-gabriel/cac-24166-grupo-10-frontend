85e603c9c25722477e344ff590b6edeaa40247cf
mikeowl <michel.b.diamant@gmail.com>
2024-05-09 09:30:05 -0300

    Se añaden double quotes en el JS

--- js/sticky.js
+++ js/sticky.js
@@ -1,11 +1,11 @@
 // Función para aplicar Sticky al navbar
-const navbar = document.querySelector('#title-bar-container');
+const navbar = document.querySelector("#title-bar-container");
 let top = navbar.offsetTop;
 function stickynavbar() {
   if (window.scrollY >= top) {    
-    navbar.classList.add('sticky');
+    navbar.classList.add("sticky");
   } else {
-    navbar.classList.remove('sticky');    
+    navbar.classList.remove("sticky");    
   }
 }
 window.addEventListener('scroll', stickynavbar);
