e1a07c7c011a71d21fbe590959ee06efadae93d5
mikeowl <michel.b.diamant@gmail.com>
2024-05-09 11:37:38 -0300

    Applying single quotes to change sticky behavior

--- js/sticky.js
+++ js/sticky.js
@@ -3,9 +3,9 @@ const navbar = document.querySelector("#title-bar-container");
 let top = navbar.offsetTop;
 function stickynavbar() {
   if (window.scrollY >= top) {    
-    navbar.classList.add("sticky");
+    navbar.classList.add('sticky');
   } else {
-    navbar.classList.remove("sticky");    
+    navbar.classList.remove('sticky');    
   }
 }
 window.addEventListener('scroll', stickynavbar);
